5f5610bea8ce3ac6a48f34d2490a959a252f0125
ddjole99 <djole.3453@gmail.com>
2025-04-01 17:50:50 +0200

    update klase prijava, dodate funkcije get, delete , add, update

--- model/prijava.php
+++ model/prijava.php
@@ -21,9 +21,37 @@ class Prijava
         return $conn->query($query);
     }
 
+
+    public static function getById($id, mysqli $conn)
+    {
+        $query = "SELECT * FROM prijave where id=$id";
+        $myArray = array();
+        $rezultat = $conn->query($query);
+        if ($rezultat) {
+            while ($red = $rezultat->fetch_array()) {
+                $myArray[] = $red;
+            }
+        }
+        return $myArray;
+    }
+
+    public function deleteByID(mysqli $conn)
+    {
+        $query = "DELETE FROM prijave WHERE id=$this->id";
+        return $conn->query($query);
+    }
+
+    public static function add(Prijava $prijava, mysqli $conn)
+    {
+        $query = "INSERT INTO prijave(predmet,katedra,sala,datum) VALUES ('$prijava->predmet','$prijava->katedra','$prijava->sala','$prijava->datum')";
+        return $conn->query($query);
+    }
+
+    public function update(mysqli $conn)
+    {
+        $query = "UPDATE prijave SET predmet='$this->predmet', katedra='$this->katedra', sala='$this->sala', datum='$this->datum'";
+        return $conn->query($query);
+    }
+
 }
-
-
-
-
 ?>
